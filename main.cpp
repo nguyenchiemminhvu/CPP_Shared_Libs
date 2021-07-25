34df536f998337ef43dc51956de06e3478b5ebf7
NCMV <nguyenchiemminhvu@gmail.com>
2021-07-25 22:31:51 +0700

    update

--- main.cpp
+++ main.cpp
@@ -1,17 +1,33 @@
 #include <unistd.h>
 #include <dlfcn.h>
 #include <iostream>
+#include <map>
 #include "example.h"
 #include "example.hpp"
 #include "example_dl.h"
 
+void * dl_lib;
+
 void __attribute__((constructor)) on_program_startup()
 {
     std::cout << "main startup" << std::endl;
+
+    dl_lib = dlopen("libexampledl.so", RTLD_NOW);
+    if (dl_lib == NULL)
+    {
+        std::cout << dlerror() << std::endl;
+        exit(-1);
+    }
 }
 
 void __attribute__((destructor)) on_program_endup()
 {
+    if (dlclose(dl_lib))
+    {
+        std::cout << dlerror();
+        exit(-1);
+    }
+
     std::cout << "main endup" << std::endl;
 }
 
@@ -19,13 +35,6 @@ int main()
 {
     Foo();
     Bar();
-
-    void * dl_lib = dlopen("libexampledl.so", RTLD_NOW);
-    if (dl_lib == NULL)
-    {
-        std::cout << dlerror() << std::endl;
-        return -1;
-    }
 
     void (*lib_func)(void *) = (void (*)(void *))dlsym(dl_lib, "dl_func");
     if (lib_func == NULL)
@@ -35,12 +44,6 @@ int main()
     }
 
     lib_func(NULL);
-
-    if (dlclose(dl_lib))
-    {
-        std::cout << dlerror();
-        return -3;
-    }
 
     return 0;
 }
